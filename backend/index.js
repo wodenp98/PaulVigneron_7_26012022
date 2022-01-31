a7a881d7cfbc1465e914ea21ea27e33aea290ec0
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-01-31 16:41:04 +0100

    Création bdd et model user

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
 const app = express();
 
 
+const db = require('./models')
 
-module.exports = app;
+// Routes
+const userRoute = require('./routes/User')
+app.use("/user", userRoute)
+
+
+db.sequelize.sync().then((req) => {
+    app.listen(3001, () => {
+        console.log("server running");
+    });
+})
+
+app.use(express.json())
+
