8810cb9689325760aa3c922f58ebf182c3c11973
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-02-01 18:21:08 +0100

    Endpoints et démarrage du front

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
+app.use(express.json());
+app.use(cors());
 
 const db = require('./models')
 
-// Routes
-const userRoute = require('./routes/User')
-app.use("/user", userRoute)
+// Routers
+const userRouter = require('./routes/User');
+app.use("/user", userRouter);
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter);
 
 
-db.sequelize.sync().then((req) => {
+
+db.sequelize.sync().then(() => {
     app.listen(3001, () => {
-        console.log("server running");
+        console.log("Server running on port 3001");
     });
 })
 
-app.use(express.json())
+
 
