e767f25a8ed1b4869dfed05a46ed5f322ebf8fbd
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-02-08 17:56:58 +0100

    Intégration d'une page d'erreur

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import { AuthContext } from "./helpers/AuthContext";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -70,6 +71,7 @@ function App() {
             <Route path="/post/:id"element={<Post/>} />
             <Route path="/registration" element={<Registration/>} />
             <Route path="/login" element={<Login/>} />
+            <Route path="*" element={<PageNotFound/>} />
           </Routes>
         </Router>
       </AuthContext.Provider>
