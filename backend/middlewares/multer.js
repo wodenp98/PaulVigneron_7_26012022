796b823e010865c5c5fc8c9b3fbc3595d703e079
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-02-18 12:01:40 +0100

    importation et configuration de multer

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif'
+};
+
+const storage = multer.diskStorage({
+  // dossier images
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  // fichier
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_'); 
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({storage: storage}).single('image');
