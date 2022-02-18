3b22c111156bb19ebafa84beff39a7cfcbe68302
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-02-18 19:11:33 +0100

    images et ignore

--- backend/models/Posts.js
+++ backend/models/Posts.js
@@ -1,11 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
 
 	const Posts = sequelize.define('Posts', {
-		id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
+		// id: {
+		// 	type: DataTypes.INTEGER,
+		// 	primaryKey: true,
+		// 	autoIncrement: true
+		// },
 		
 		title: {
 			type: DataTypes.STRING,
