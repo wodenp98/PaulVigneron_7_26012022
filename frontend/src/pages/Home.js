84b36ea75b1384898fcd90282dcbfdf0a7d8b50d
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-03-09 14:26:59 +0100

    icone commentaire

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
+import CommentIcon from '@mui/icons-material/Comment';
 
 
 function Home() {
@@ -113,7 +114,9 @@ function Home() {
               <div className="username"><Link to ={`/profile/${value.UserId}`}>{value.username}</Link>
               </div>
               <label>{dateParser(value.createdAt)}</label>
+              
               <div className="buttons">
+                <CommentIcon />
                 <ThumbUpAltIcon
                   onClick={() => {
                     likeAPost(value.id);
